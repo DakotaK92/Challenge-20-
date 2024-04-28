12a44fb8f8e4187d47246400e3926fb264aa5f7c
Dakota.King <dakotaking92@gmail.com>
2024-04-28 17:36:19 -0500

    Rename files on main

--- client/src/utils/auth.jsx
+++ client/src/utils/auth.jsx
@@ -1,25 +1,25 @@
 // use this to decode and get user's information out of it
 import decode from 'jwt-decode';
 
-// create a new class to instantaite for a user
+// create a new class to instantiate for a user
 class AuthService {
     // get user data
-    getProfile () {
+    getProfile() {
         return decode(this.getToken());
     }
 
     // check if user's logged in
-    loggedIn () {
-        // Checks if there is a saved and it's till valid
+    loggedIn() {
+        // Checks if there is a saved token and it's still valid
         const token = this.getToken();
-        return !! token && !this.isTokenExpired(token); // handwaiving here
+        return !!token && !this.isTokenExpired(token); // handwaiving here
     }
 
     // check if token is expired
-    isTokenExpired (token) {
+    isTokenExpired(token) {
         try {
-            const decoded = decode(token);
-            if (decoded.exp < DataTransfer.now / 1000) {
+        const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
                 return true;
             } else return false;
         } catch (err) {
@@ -27,19 +27,19 @@ class AuthService {
         }
     }
 
-    getToken () {
-        // Retrieves the user to localStorage
+    getToken() {
+        // Retrieves the user token from localStorage
         return localStorage.getItem('id_token');
     }
-    
+
     login(idToken) {
-        // Saves user token from localStorage
-        return localStorage.setItem('id_token', idToken);
+        // Saves user token to localStorage
+        localStorage.setItem('id_token', idToken);
         window.location.assign('/');
     }
 
     logout() {
-        // clear user token and profile data from localStorage
+        // Clear user token and profile data from localStorage
         localStorage.removeItem('id_token');
         // this will reload the page and reset the state of the application
         window.location.assign('/');
