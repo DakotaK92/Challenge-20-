f7112da31a71402af6a8ded5420c1a217b2517fe
Dakota.King <dakotaking92@gmail.com>
2024-04-28 20:06:05 -0500

    Update cliient files on main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-dom/client';
-import SearchBooks from "./pages/SerachBooks.jsx";
-import SavedBooks from "./pages/SavedBooks.jsx";
-import Navbar from "./components/Navbar.jsx";
+import './App.css';
+import { Outlet } from 'react-router-dom';
 
-// import Apollo Provider
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-
-// Construct main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-// Construct request middleware that will attch the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-// excutre the 'authLink middleware prior to making the request to the GraphQL API
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <ApolloProvider client = {client}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path= "/" component={SearchBooks} />
-            <Route exact path= "/saved" component={SavedBooks} />
-            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
-    </ApolloProvider>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
