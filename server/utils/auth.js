3c37c804d529c22b5deb46840d8978c7bd524678
DakotaK92 <46942706+DakotaK92@users.noreply.github.com>
2024-05-03 08:58:44 -0500

    Update js on main

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,17 @@
 const jwt = require('jsonwebtoken');
-
-// set token secret and expiration date
+const { GraphQLError } = require('graphql');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -19,7 +22,6 @@ module.exports = {
       return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
